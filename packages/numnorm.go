28e72d9eb5cac1c026a6a141de796821fa56261b
Vladimir Ulogov <vladimir.ulogov@me.com>
2020-11-17 19:16:03 -0800

    num/norm

---
+++ packages/numnorm.go
@@ -0,0 +1,40 @@
+package packages
+
+import (
+  "reflect"
+  "github.com/mattn/anko/env"
+  stat "gonum.org/v1/gonum/stat"
+  floats "gonum.org/v1/gonum/floats"
+)
+
+func NumNorm(data []float64) []float64 {
+  res := make([]float64, len(data))
+  xmin := floats.Min(data)
+  xmax := floats.Max(data)
+  diff := xmax-xmin
+  for i:=0;i<len(data);i++ {
+    res[i] = (data[i] - xmin)/diff
+  }
+  return res
+}
+
+func NumStand(data []float64) []float64 {
+  xmean := stat.Mean(data, nil)
+  xdev  := stat.StdDev(data, nil)
+  res := make([]float64, len(data))
+  for i:=0;i<len(data);i++ {
+    res[i] = (data[i] - xmean)/xdev
+  }
+  return res
+}
+
+
+func init() {
+  env.Packages["num/norm"] = map[string]reflect.Value{
+    "Normalize":                reflect.ValueOf(NumNorm),
+    "Standartize":              reflect.ValueOf(NumStand),
+  }
+  env.PackageTypes["num/norm"] = map[string]reflect.Type{
+
+  }
+}
