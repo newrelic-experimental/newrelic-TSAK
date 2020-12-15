2b05e5668d73ce5ce13b5ae6c4a763597af88842
Vladimir Ulogov <vladimir.ulogov@me.com>
2020-12-14 22:28:48 -0800

    Module: conf

---
+++ packages/conf.go
@@ -0,0 +1,53 @@
+package packages
+
+import (
+  "reflect"
+  "github.com/mattn/anko/env"
+  "github.com/newrelic-experimental/newrelic-TSAK/internal/conf"
+)
+
+func ConfNRAPI(val string) string {
+  conf.Nrapi = val
+  return conf.Nrapi
+}
+
+func ConfNrapi() string {
+  return conf.Nrapi
+}
+
+func ConfNRAPIQ(val string) string {
+  conf.Nrapiq = val
+  return conf.Nrapiq
+}
+
+func ConfNrapiq() string {
+  return conf.Nrapiq
+}
+
+func ConfACCOUNT(val string) string {
+  conf.Account = val
+  return conf.Account
+}
+
+func ConfAccount() string {
+  return conf.Account
+}
+
+
+func init() {
+  env.Packages["conf"] = map[string]reflect.Value{
+    "NRAPI":           reflect.ValueOf(ConfNRAPI),
+    "Nrapi":           reflect.ValueOf(ConfNrapi),
+    "NRAPIQ":          reflect.ValueOf(ConfNRAPIQ),
+    "Nrapiq":          reflect.ValueOf(ConfNrapiq),
+    "ACCOUNT":         reflect.ValueOf(ConfACCOUNT),
+    "Account":         reflect.ValueOf(ConfAccount),
+    "Version":         reflect.ValueOf(conf.Ver),
+    "VersionMajor":    reflect.ValueOf(conf.VerMaj),
+    "VersionMinor":    reflect.ValueOf(conf.VerMin),
+    "VersionPrerelease":reflect.ValueOf(conf.VerPrerelease),
+  }
+  env.PackageTypes["conf"] = map[string]reflect.Type{
+
+  }
+}
