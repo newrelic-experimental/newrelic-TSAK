ab0b4aa4afade830917e231d8bc57378683c144c
Vladimir Ulogov <vladimir.ulogov@me.com>
2020-12-17 03:56:00 -0800

    message signature support

---
+++ packages/cryptothemismessage.go
@@ -0,0 +1,16 @@
+package packages
+
+import (
+  "reflect"
+  "github.com/mattn/anko/env"
+  message "github.com/cossacklabs/themis/gothemis/message"
+)
+
+func init() {
+  env.Packages["crypto/themis/message"] = map[string]reflect.Value{
+    "New":                    reflect.ValueOf(message.New),
+  }
+  env.PackageTypes["crypto/themis/keys"] = map[string]reflect.Type{
+    "SecureMeaages":          reflect.TypeOf(message.SecureMessage{}),
+  }
+}
