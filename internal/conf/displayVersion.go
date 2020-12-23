b42737858576927843ccbff4e729eb8c8a0d2083
Vladimir Ulogov <vladimir.ulogov@me.com>
2020-12-23 14:24:56 -0800

    extended -version

--- internal/conf/displayVersion.go
+++ internal/conf/displayVersion.go
@@ -4,14 +4,33 @@ import (
   "os"
   "fmt"
   "github.com/olekukonko/tablewriter"
+  "github.com/shirou/gopsutil/v3/host"
 )
 
 func DisplayVersion() {
-  data  := [][]string{
-    []string{"TSAK version", Ver},
-    []string{"TSAK version major", fmt.Sprintf("%v",VerMaj)},
-    []string{"TSAK version minor", fmt.Sprintf("%v",VerMin)},
-    []string{"TSAK version prerelease", fmt.Sprintf("%v",VerPrerelease)},
+  var data [][]string
+  info, err  := host.Info()
+  if err != nil {
+    data  = [][]string{
+      []string{"TSAK version", Ver},
+      []string{"TSAK version major", fmt.Sprintf("%v",VerMaj)},
+      []string{"TSAK version minor", fmt.Sprintf("%v",VerMin)},
+      []string{"TSAK version prerelease", fmt.Sprintf("%v",VerPrerelease)},
+    }
+  } else {
+    data  = [][]string{
+      []string{"TSAK version", Ver},
+      []string{"TSAK version major", fmt.Sprintf("%v",VerMaj)},
+      []string{"TSAK version minor", fmt.Sprintf("%v",VerMin)},
+      []string{"TSAK version prerelease", fmt.Sprintf("%v",VerPrerelease)},
+      []string{"Host OS",           info.OS},
+      []string{"Platform",          info.Platform},
+      []string{"Platform family",   info.PlatformFamily},
+      []string{"Platform version",  info.PlatformVersion},
+      []string{"Kernel version",    info.KernelVersion},
+      []string{"Architecture",      info.KernelArch},
+
+    }
   }
   table := tablewriter.NewWriter(os.Stdout)
   for _, v := range data {
