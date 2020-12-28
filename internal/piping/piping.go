29a7313d985f6be633817a6485c4ef5781bafd3c
Vladimir Ulogov <vladimir.ulogov@me.com>
2020-12-28 13:49:51 -0800

    Remove pipeline statistics collection as I am doing it wrong.

--- internal/piping/piping.go
+++ internal/piping/piping.go
@@ -3,10 +3,7 @@ package piping
 import (
   "fmt"
   "bytes"
-  "github.com/sirupsen/logrus"
   "github.com/newrelic-experimental/newrelic-TSAK/internal/log"
-  "github.com/newrelic-experimental/newrelic-TSAK/internal/conf"
-  humanize "github.com/dustin/go-humanize"
   zmq "github.com/pebbe/zmq4"
 )
 
@@ -27,57 +24,22 @@ var zmqS = make(map[string]*zmq.Socket)
 var zmqCtx,_ = zmq.NewContext()
 var zmqErr int64
 
-var N   = make(map[int]int)
-var Nb  = make(map[int]int)
-var Ns  = make(map[int]int)
 
 func To(dst int, _data []byte) {
   var data = bytes.NewBuffer(_data)
-  _, ok := Nb[dst]
-  if ! ok {
-    Nb[dst] = 0
-  }
-  Nb[dst] += data.Len()
-  _, ok = Ns[dst]
-  if ! ok {
-    Ns[dst] = 0
-  }
-  _, ok = N[dst]
-  if ! ok {
-    N[dst] = 0
-  }
+
   if dst == INCH {
     pipeIn <- data.String()
-    Ns[dst] = len(pipeIn)
   } else if dst == OUTCH {
     pipeOut <- data.String()
-    Ns[dst] = len(pipeOut)
   } else if dst == CLIPS {
     clipsIn <- data.String()
-    Ns[dst] = len(clipsIn)
   } else if dst == FACTS {
     factsIn <- data.String()
-    Ns[dst] = len(factsIn)
   } else if dst == EVAL {
     evalIn <- data.String()
-    Ns[dst] = len(evalIn)
   } else {
     log.Error("Trying to send data to non-existent pipeline")
-  }
-  if N[dst] > conf.Every {
-    sNb := humanize.Bytes(uint64(Nb[dst]))
-    log.Trace("PIPELINE statistics", logrus.Fields{
-      "pipeline":       dst,
-      "submitted":      N[dst],
-      "bytes":          Nb[dst],
-      "bytesH":         sNb,
-      "elements":       Ns[dst],
-    })
-    N[dst]  = 0
-    Ns[dst] = 0
-    Nb[dst] = 0
-  } else {
-    N[dst] += 1
   }
 }
 
