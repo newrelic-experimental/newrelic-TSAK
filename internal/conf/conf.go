fd7bf40bd756954bbc64cd000e5fab11d306b820
Vladimir Ulogov <vladimir.ulogov@me.com>
2020-12-29 13:48:31 -0800

    0.3-pre5

--- internal/conf/conf.go
+++ internal/conf/conf.go
@@ -33,8 +33,8 @@ var EventType string
 var AppPath string
 var IsVersion bool
 var IsStop bool
-var Ver = "0.3-pre4"
+var Ver = "0.3-pre5"
 var VerMaj = 0
 var VerMin = 3
-var VerPrerelease = 4
+var VerPrerelease = 5
 var Args []string
